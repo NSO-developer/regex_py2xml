d58cf72eef43ea6723f1b92aece34722fa1ddde6
Jan Lindblad <jlindbla@cisco.com>
2017-04-05 09:19:41 +0200

    Initial implementation of negative patterns

--- clean-pattern.py
+++ clean-pattern.py
@@ -50,12 +50,13 @@ def hunt_patterns(stmts):
         if 'pattern' == stmt.keyword:
             orig_pattern = stmt.arg
             log("%s: Parsing pattern '%s'"%(stmt.pos, orig_pattern),)
-            new_pattern  = translate(stmt.pos, orig_pattern)
+            patterns = translate(stmt.pos, orig_pattern)
             # Translation will sometimes generate multiple successive .*
             # patterns, which can be simplified with a single .*
-            while ".*.*" in new_pattern:
-                new_pattern = new_pattern.replace(".*.*", ".*")
-            if new_pattern == orig_pattern:
+            for i in xrange(len(patterns)):
+                while ".*.*" in patterns[i]:
+                    patterns[i] = patterns[i].replace(".*.*", ".*")
+            if len(patterns) == 1 and patterns[0] == orig_pattern:
                 # Translation yields the original string, 
                 # so let's not talk about this pattern
                 pass
@@ -66,8 +67,11 @@ def hunt_patterns(stmts):
                     (stmt.pos, confidence_str)
                 print "    pattern '%s'"%stmt.arg
                 print "    If it is, this is how I would translate it"\
-                      " to a YANG pattern statement:"
-                print "    pattern '%s'"%new_pattern
+                      " to YANG pattern statements:"
+                invert = ""
+                for pat in patterns:
+                    print "    pattern '%s'%s"%(pat, invert)
+                    invert = " { modifier invert-match; }"
         if stmt.substmts:
             hunt_patterns(stmt.substmts)
 
@@ -84,8 +88,17 @@ def get_confidence_str(pattern):
 
 def translate(fileline, pyre):
     parse_tree = sre_parse.parse(pyre)
-    (fragments, _anchors) = collect(fileline, [('TOP', (None, [parse_tree]))])
-    return "".join(fragments)
+    patterns = []
+    max_negative = 0
+    while max_negative < 5:
+        log("Starting tree walk with max_negative=%s"%max_negative)
+        (fragments, _anchors, _hit_neg) = \
+            collect(fileline, [('TOP', (None, [parse_tree]))], max_negative=max_negative)
+        if not fragments:
+            break
+        patterns += ["".join(fragments)]
+        max_negative += 1
+    return patterns
 
 def logerror(fileline, str):
     print "%s: ERROR: %s" % (fileline, str)
@@ -98,44 +111,49 @@ def log(str, indent=0):
     if clean_pattern_trace:
         print "%02s... %s%s" % (indent, indent*" ", str)
 
-def collect(fileline, body, depth=0, head_flex=True, tail_flex=True):
+def collect(fileline, body, depth=0, head_flex=True, tail_flex=True, max_negative=0):
     inner = []
     head_anchor = tail_anchor = False
     for index in xrange(len(body)):
         subnode = body[index]
         is_first = (0 == index)
         is_last  = (len(body)-1 == index)
+        neg_hit = False
         result = generate(fileline, subnode, depth+1, \
             is_first and head_flex, 
-            is_last  and tail_flex)
+            is_last  and tail_flex,
+            max_negative)
         # result is either a string (xml schema regex pattern)
         # or a tuple with found string and anchors, (pattern, (head, tail))
         if isinstance(result, tuple): # anchor tuple
-            (fragment, (result_head_anchor, result_tail_anchor)) = result
+            (fragment, (result_head_anchor, result_tail_anchor), neg_hit) = result
             head_anchor = max(head_anchor, result_head_anchor)
             tail_anchor = max(tail_anchor, result_tail_anchor)
         else: # fragment string
             fragment = result
         inner += fragment
-    return (inner, (head_anchor, tail_anchor))
+        if neg_hit:
+            log("neg hit, breaking", depth)
+            break
+    return (inner, (head_anchor, tail_anchor), neg_hit)
 
-def generate(fileline, node, depth, head_flex, tail_flex):
+def generate(fileline, node, depth, head_flex, tail_flex, max_negative):
     if isinstance(node, tuple):
         key = node[0]
         if 'at' == key:
             position = node[1]
             log("at %s" % position, depth)
             if "at_beginning" == position:
-                return ("", (True, False)) # anchor at beginning: ^
-            return ("", (False, True))     # anchor at end      : $
+                return ("", (True, False), False) # anchor at beginning: ^
+            return ("", (False, True), False)     # anchor at end      : $
         elif 'max_repeat' == key:
             rmin, rmax = node[1][0], node[1][1]
             body = node[1][2]
             log("max_repeat %s %s" % (rmin, rmax), depth)
             if rmax > 1:
                 head_flex = tail_flex = False
-            (fragments, (head_anchor, tail_anchor)) = \
-                collect(fileline, body, depth, head_flex, tail_flex)
+            (fragments, (head_anchor, tail_anchor), neg_hit) = \
+                collect(fileline, body, depth, head_flex, tail_flex, max_negative)
             if (head_anchor or tail_anchor) and rmax > 1:
                 logwarning(fileline, "Using an anchor (^$) inside a "\
                     "repetition group which allows more than one repetition "\
@@ -143,46 +161,48 @@ def generate(fileline, node, depth, head_flex, tail_flex):
                     "as if anchor was outside repetition group.")
             inner = "".join(fragments)
             if rmin == 0 and rmax == 1:
-                return (inner + "?", (head_anchor, tail_anchor))
+                return (inner + "?", (head_anchor, tail_anchor), neg_hit)
             if rmin == 0 and rmax == 4294967295:
-                return (inner + "*", (head_anchor, tail_anchor))
+                return (inner + "*", (head_anchor, tail_anchor), neg_hit)
             if rmin == 1 and rmax == 4294967295:
-                return (inner + "+", (head_anchor, tail_anchor))
+                return (inner + "+", (head_anchor, tail_anchor), neg_hit)
             if rmin == rmax:
-                return (inner + "{%s}" % rmin, (head_anchor, tail_anchor))
-            return (inner + "{%s,%s}" % (rmin, rmax), (head_anchor, tail_anchor))
+                return (inner + "{%s}" % rmin, (head_anchor, tail_anchor), neg_hit)
+            return (inner + "{%s,%s}" % (rmin, rmax), (head_anchor, tail_anchor), neg_hit)
         elif 'subpattern' == key:
             body = node[1][1]
             log("subpattern %s" % node[1][0], depth)
             #log("aaa %s"%((head_flex, tail_flex),), depth)
-            (fragments, anchors) = collect(fileline, body, depth, head_flex, tail_flex)
+            (fragments, anchors, neg_hit) = collect(fileline, body, depth, head_flex, tail_flex, max_negative)
             #log("bbb %s"%(anchors,), depth)
             if len(body) == 1 and body[0][0] != 'branch':
                 # A subpattern with a single element is redundant
-                return ("".join(fragments), anchors)
-            return ("(" + "".join(fragments) + ")", anchors)
+                return ("".join(fragments), anchors, neg_hit)
+            return ("(" + "".join(fragments) + ")", anchors, neg_hit)
         elif 'branch' == key or 'TOP' == key:
             body = node[1][1]
             log("branch %s" % str(node[1][0]), depth)
             inner = []
-            head_anchor = tail_anchor = False
+            neg_hit = head_anchor = tail_anchor = False
             for branch in body:
                 log("", depth) # Empty line in between branch alternatives
-                (fragments, (result_head_anchor, result_tail_anchor)) = \
-                    collect(fileline, branch, depth, head_flex, tail_flex)
+                (fragments, (result_head_anchor, result_tail_anchor), result_neg_hit) = \
+                    collect(fileline, branch, depth, head_flex, tail_flex, max_negative)
                 result = "".join(fragments)
                 if head_flex and not result_head_anchor:
                     result = ".*" + result
                 if tail_flex and not result_tail_anchor:
                     result += ".*"
-                inner += [result]
+                if 0 == max_negative or result_neg_hit:
+                    inner += [result]
+                neg_hit = max(neg_hit, result_neg_hit)
                 head_anchor = max(head_anchor, result_head_anchor)
                 tail_anchor = max(tail_anchor, result_tail_anchor)
-            return ("|".join(inner), (head_anchor, tail_anchor))
+            return ("|".join(inner), (head_anchor, tail_anchor), neg_hit)
         elif 'in' == key:
             body = node[1]
             log("in %s" % len(body), depth)
-            (fragments, anchors) = collect(fileline, body, depth, head_flex, tail_flex)
+            (fragments, anchors, neg_hit) = collect(fileline, body, depth, head_flex, tail_flex, max_negative)
             if len(body) == 1 and 'category' == body[0][0]:
                 # 'in' with single category as arg can be represented 
                 # without the []
@@ -216,23 +236,25 @@ def generate(fileline, node, depth, head_flex, tail_flex):
             try: 
                 return cat_dict[cat_name]
             except:
-                logerror(fileline, "Unhandled character category: %s" % cat_name)
+                logerror(fileline, "Parsing failed, unknown character category: %s" % cat_name)
         elif 'assert_not' == key:
-            assertion = str(node[1])
-            logwarning(fileline, "Assertion ignored: %s" % str(assertion))
-            return ""
+            body = node[1][1]
+            length = node[1][0]
+            if 1 != max_negative:
+                log("assert_not %s skipped in current run" % length, depth)
+                return ""
+            log("assert_not %s in negative pattern" % length, depth)
+            (fragments, anchors, neg_hit) = collect(fileline, body, depth, head_flex, tail_flex, max_negative-1)
 
-        elif 'TOP' == key:
-            body = node[1]
-            log("TOP %s" % len(body), depth)
-            (fragments, anchors) = collect(fileline, body, depth, head_flex, tail_flex)
-            return "".join(fragments)
-
+            if len(body) == 1 and body[0][0] != 'branch':
+                # A subpattern with a single element is redundant
+                return ("".join(fragments), anchors, True)
+            return ("".join(fragments), anchors, True)
         else:
-            logerror(fileline, "Unhandled tuple node: %s len %s" % (node[0], len(node)))
+            logerror(fileline, "Parsing failed, unknown tuple node: %s len %s" % (node[0], len(node)))
             return "#ERROR#"
     else:
-        logerror(fileline, "Unhandled node: %s" % node)
+        logerror(fileline, "Parsing failed, unknown node: %s" % node)
         return "#ERROR#"
 
 cat_dict = {
